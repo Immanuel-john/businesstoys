15c9cf35761fc5757eed4fca94c47225e801c719
Immanuel-john <imjjohn18@gmail.com>
2018-03-12 21:24:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {TabMenuModule} from 'primeng/tabmenu';
+import { RouterModule } from '@angular/router';
+import {PanelModule} from 'primeng/panel';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import {MenuModule} from 'primeng/menu';
+import {TabViewModule} from 'primeng/tabview';
+import {ButtonModule} from 'primeng/button';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path:'',component:UserComponent}]),
+    TabMenuModule,
+    MenuModule,
+    PanelModule,
+    TabViewModule,
+    ButtonModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+
+    ngOnInit() {
+
+    }
+
+}
